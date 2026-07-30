da88a68400512f47b820efd3c5db39107f296925
Maks <maks.topy.tatoo@gmail.com>
2026-07-30 08:38:53 -0300

    дебаг

--- plugins/withBundleInDebug.js
+++ plugins/withBundleInDebug.js
@@ -1,17 +1,18 @@
 const { withAppBuildGradle } = require('@expo/config-plugins');
 
 /**
- * Expo config plugin that adds `bundleInDebug = true` to `android/app/build.gradle`.
- * This forces Expo to bundle JS assets in Debug builds, allowing Debug APKs
- * to run standalone on physical devices without needing a running Metro server.
+ * Expo config plugin that sets `debuggableVariants = []` in `android/app/build.gradle`.
+ * By default, React Native Gradle Plugin skips bundling JS for 'debuggableVariants' (which defaults to ['debug']).
+ * Setting `debuggableVariants = []` forces Gradle to build and bundle JS/assets into Debug APKs,
+ * allowing Debug APKs to work standalone without Metro server.
  */
 module.exports = function withBundleInDebug(config) {
   return withAppBuildGradle(config, (config) => {
     if (config.modResults.language === 'groovy') {
-      if (!config.modResults.contents.includes('bundleInDebug = true')) {
+      if (!config.modResults.contents.includes('debuggableVariants = []')) {
         config.modResults.contents = config.modResults.contents.replace(
           /react\s*\{/,
-          'react {\n    bundleInDebug = true'
+          'react {\n    debuggableVariants = []'
         );
       }
     }
